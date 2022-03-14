1ac176b582279ba49f48a196e838aded7f8fc540
Minyong Oh <dhalsdyd@gmail.com>
2022-03-14 18:40:08 +0900

    border 없애기

--- application/lib/screens/trade_setting.dart
+++ application/lib/screens/trade_setting.dart
@@ -16,144 +16,146 @@ class TradeSetting extends GetWidget<TradeController> {
     _height = MediaQuery.of(context).size.height;
     _width = MediaQuery.of(context).size.width;
 
-
     return Scaffold(
       backgroundColor: blueOne,
       body: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Container(width: _width, height: _height),
-            Positioned(
-              top: _height * 0.15,
-              left: _width * 0.1,
-              child: Text("현재 매매 상황", style: tradeSettingNowStatusTitle),
-            ),
-            Positioned(
-              top: _height * 0.15 + 25,
-              left: _width * 0.1,
-              child: Obx(() {
-                bool isTrade = controller.userController.user.isTradeOn as bool;
+          child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(width: _width, height: _height),
+          Positioned(
+            top: _height * 0.15,
+            left: _width * 0.1,
+            child: Text("현재 매매 상황", style: tradeSettingNowStatusTitle),
+          ),
+          Positioned(
+            top: _height * 0.15 + 25,
+            left: _width * 0.1,
+            child: Obx(() {
+              bool isTrade = controller.userController.user.isTradeOn as bool;
 
-                return Text(isTrade ? "자동매매 진행중" : "투자중이 아닙니다", style: tradeSettingNowStatus);
-              }),
-            ),
-            Positioned(
-              top: _height * 0.325,
-              left: _width * 0.1,
-              child: Row(
-                children: [
-                  Text("매매 사용하기", style: tradeSettingMenuTitle),
-                  SizedBox(width: _width * 0.05),
-                  FutureBuilder(
-                      future: controller.checkUserTradeStatus(),
-                      builder: (BuildContext context, AsyncSnapshot snapshot) {
-                        if (snapshot.hasData) {
-                          return Obx(() => FlutterSwitch(
-                            height: _height * 0.03,
-                            width: _width * 0.12,
-                            padding: 4.0,
-                            toggleSize: _width * 0.04,
-                            borderRadius: 16.0,
-                            activeColor: blueThree,
-                            value: controller.isTradeTurnOn.value,
-                            onToggle: (value) => controller.isTradeTurnOn.value = value,
-                          ));
-                        } else if (snapshot.hasError) { //데이터를 정상적으로 불러오지 못했을 때
-                          return Stack(
-                            alignment: Alignment.center,
-                            children: [
-                              SizedBox(width: _width, height: _height * 0.4),
-                              Center(child: Text("데이터를 정상적으로 불러오지 못했습니다. \n다시 시도해 주세요.", textAlign: TextAlign.center)),
-                            ],
-                          );
-                        } else { //데이터를 불러오는 중
-                          return Stack(
-                            alignment: Alignment.center,
-                            children: [
-                              SizedBox(width: _width * 0.1, height: _height * 0.03),
-                              Center(child: CircularProgressIndicator()),
-                            ],
-                          );
-                        }
+              return Text(isTrade ? "자동매매 진행중" : "투자중이 아닙니다",
+                  style: tradeSettingNowStatus);
+            }),
+          ),
+          Positioned(
+            top: _height * 0.325,
+            left: _width * 0.1,
+            child: Row(
+              children: [
+                Text("매매 사용하기", style: tradeSettingMenuTitle),
+                SizedBox(width: _width * 0.05),
+                FutureBuilder(
+                    future: controller.checkUserTradeStatus(),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (snapshot.hasData) {
+                        return Obx(() => FlutterSwitch(
+                              height: _height * 0.03,
+                              width: _width * 0.12,
+                              padding: 4.0,
+                              toggleSize: _width * 0.04,
+                              borderRadius: 16.0,
+                              activeColor: blueThree,
+                              value: controller.isTradeTurnOn.value,
+                              onToggle: (value) =>
+                                  controller.isTradeTurnOn.value = value,
+                            ));
+                      } else if (snapshot.hasError) {
+                        //데이터를 정상적으로 불러오지 못했을 때
+                        return Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            SizedBox(width: _width, height: _height * 0.4),
+                            Center(
+                                child: Text(
+                                    "데이터를 정상적으로 불러오지 못했습니다. \n다시 시도해 주세요.",
+                                    textAlign: TextAlign.center)),
+                          ],
+                        );
+                      } else {
+                        //데이터를 불러오는 중
+                        return Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            SizedBox(
+                                width: _width * 0.1, height: _height * 0.03),
+                            Center(child: CircularProgressIndicator()),
+                          ],
+                        );
                       }
-                  )
-                ],
-              ),
+                    })
+              ],
             ),
-            Positioned(
-              top: _height * 0.4,
-              left: _width * 0.1,
-              child: Row(
-                children: [
-                  Text("매매할 주식의 상승 정확도", style: tradeSettingMenuTitle),
-                  getNumTextField("%", controller.accuracyPercentageTextController),
-                  Text("이상", style: tradeSettingMenuTitle),
-                ],
-              ),
+          ),
+          Positioned(
+            top: _height * 0.4,
+            left: _width * 0.1,
+            child: Row(
+              children: [
+                Text("매매할 주식의 상승 정확도", style: tradeSettingMenuTitle),
+                getNumTextField(
+                    "%", controller.accuracyPercentageTextController),
+                Text("이상", style: tradeSettingMenuTitle),
+              ],
             ),
-            Positioned(
-              top: _height * 0.475,
-              left: _width * 0.1,
-              child: Row(
-                children: [
-                  getNumTextField("%", controller.tradePercentageTextController),
-                  Text("이상 상승/하락 시 매도", style: tradeSettingMenuTitle),
-                ],
-              ),
+          ),
+          Positioned(
+            top: _height * 0.475,
+            left: _width * 0.1,
+            child: Row(
+              children: [
+                getNumTextField("%", controller.tradePercentageTextController),
+                Text("이상 상승/하락 시 매도", style: tradeSettingMenuTitle),
+              ],
             ),
-            Positioned(
-              top: _height * 0.55,
-              left: _width * 0.1,
-              child: Row(
-                children: [
-                  Text("분산 투자할 주식 개수", style: tradeSettingMenuTitle),
-                  getNumTextField("입력", controller.stockAmountTextController),
-                ],
-              ),
+          ),
+          Positioned(
+            top: _height * 0.55,
+            left: _width * 0.1,
+            child: Row(
+              children: [
+                Text("분산 투자할 주식 개수", style: tradeSettingMenuTitle),
+                getNumTextField("입력", controller.stockAmountTextController),
+              ],
             ),
-            Positioned(
-              top: _height * 0.65,
-              left: _width * 0.1,
-              child: Obx(() => Row(
-                children: [
-                  Text("매매 방식", style: tradeSettingMenuTitle),
-                  SizedBox(width: _width * 0.04),
-                  getChooseBox("시장가"),
-                  SizedBox(width: _width * 0.03),
-                  getChooseBox("일반가"),
-                ],
-              )),
-            ),
-            Positioned(
-              bottom: _height * 0.15,
-              child: GestureDetector(
-                onTap: () => controller.storeTradeSetting(),
-                child: Container(
-                  width: _width * 0.45,
-                  height: _height * 0.05,
-                  decoration: BoxDecoration(
+          ),
+          Positioned(
+            top: _height * 0.65,
+            left: _width * 0.1,
+            child: Obx(() => Row(
+                  children: [
+                    Text("매매 방식", style: tradeSettingMenuTitle),
+                    SizedBox(width: _width * 0.04),
+                    getChooseBox("시장가"),
+                    SizedBox(width: _width * 0.03),
+                    getChooseBox("일반가"),
+                  ],
+                )),
+          ),
+          Positioned(
+            bottom: _height * 0.15,
+            child: GestureDetector(
+              onTap: () => controller.storeTradeSetting(),
+              child: Container(
+                width: _width * 0.45,
+                height: _height * 0.05,
+                decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(16),
                     boxShadow: [
-                      BoxShadow(
-                        blurRadius: 10,
-                        spreadRadius: 3,
-                        color: blueTwo
-                      )
-                    ]
-                  ),
-                  child: Center(child: Text("설정 저장하기")),
-                ),
+                      BoxShadow(blurRadius: 10, spreadRadius: 3, color: blueTwo)
+                    ]),
+                child: Center(child: Text("설정 저장하기")),
               ),
-            )
-          ],
-        )
-      ),
+            ),
+          )
+        ],
+      )),
     );
   }
 
-  SizedBox getNumTextField(String hintText, TextEditingController textController) {
+  SizedBox getNumTextField(
+      String hintText, TextEditingController textController) {
     return SizedBox(
       width: _width * 0.12,
       child: TextField(
@@ -162,6 +164,8 @@ class TradeSetting extends GetWidget<TradeController> {
         textAlign: TextAlign.center,
         style: tradeSettingMenuTextField,
         decoration: InputDecoration(
+          border: InputBorder.none,
+          focusedBorder: InputBorder.none,
           contentPadding: EdgeInsets.symmetric(vertical: _height * 0.02),
           hintText: hintText,
         ),
@@ -186,9 +190,11 @@ class TradeSetting extends GetWidget<TradeController> {
                 blurRadius: 5,
                 spreadRadius: 1,
               )
-            ]
-        ),
-        child: Center(child: Text(btnText, style: TextStyle(color: isChoose ? Colors.white : Colors.black))),
+            ]),
+        child: Center(
+            child: Text(btnText,
+                style:
+                    TextStyle(color: isChoose ? Colors.white : Colors.black))),
       ),
     );
   }
