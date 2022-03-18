474b6ef4534b61059a0b02f749c0737900b584b4
Minyong Oh <dhalsdyd@gmail.com>
2022-03-18 10:46:21 +0900

    separate func

--- application/lib/controllers/stock_controller.dart
+++ application/lib/controllers/stock_controller.dart
@@ -6,21 +6,23 @@ class StockController extends GetxController with StateMixin {
 
   //StockChart chart;
   //StockHistory hist;
-
-  RxString ticker = "".obs;
-
   Rx<StockInfo> info = Rx(StockInfo());
   Rx<StockQuote> price = Rx(StockQuote());
 
   @override
   void onInit() async {
+    super.onInit();
+  }
+
+  getStock(String ticker) async {
     change(null, status: RxStatus.loading());
-    String symbol = ticker.value + ".KS";
-    (await yfin.checkSymbol(symbol)) ? null : symbol = ticker.value + ".KQ";
+    String symbol = ticker + ".KS";
+
+    (await yfin.checkSymbol(symbol)) ? null : symbol = ticker + ".KQ";
 
     info.value = yfin.getStockInfo(ticker: symbol);
+
     price.value = await yfin.getPrice(stockInfo: info.value);
     change(null, status: RxStatus.success());
-    super.onInit();
   }
 }
