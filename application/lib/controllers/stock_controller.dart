115a897936dd0b2b3738ca475fd386d732a13c34
Minyong Oh <dhalsdyd@gmail.com>
2022-03-19 20:25:33 +0900

    Change : stock_controller Structure

--- application/lib/controllers/stock_controller.dart
+++ application/lib/controllers/stock_controller.dart
@@ -33,17 +33,4 @@ class StockController extends GetxController with StateMixin {
     price.value = await yfin.getPrice(stockInfo: info.value);
     change(null, status: RxStatus.success());
   }
-
-  getRealtimeStock() {
-    if (isRealtime.isFalse) {
-      isRealtime.value = true;
-
-      timer = Timer.periodic(const Duration(seconds: 1), (timer) async {
-        price.value = await yfin.getPrice(stockInfo: info.value);
-      });
-    } else {
-      isRealtime.value = false;
-      timer!.cancel();
-    }
-  }
 }
