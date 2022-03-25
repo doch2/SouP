4538230d6cfdabd78f84696ba33327da8e49d0f5
Minyong Oh <dhalsdyd@gmail.com>
2022-03-25 23:58:32 +0900

    Remove : unnecessary code

--- application/lib/screens/home.dart
+++ application/lib/screens/home.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_background_service/flutter_background_service.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:soup/controllers/user_controller.dart';
 import 'package:soup/models/stock.dart';
@@ -82,81 +80,6 @@ class Home extends StatelessWidget {
                 StockModel(stockId: "089980", name: "상아프론테크"),
               ],
               recommanded: false,
-            ),
-          ),
-          Positioned(
-            top: _height * 0.725,
-            left: _width * 0.1,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                StreamBuilder<Map<String, dynamic>?>(
-                  stream: FlutterBackgroundService().onDataReceived,
-                  builder: (context, snapshot) {
-                    print("Snapshot $snapshot");
-
-                    if (!snapshot.hasData) {
-                      FlutterBackgroundService()
-                          .sendData({"action": "getAgain"});
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-
-                    final data = snapshot.data!;
-                    String? device = data["device"];
-                    DateTime? date = DateTime.tryParse(data["current_date"]);
-                    return Column(
-                      children: [
-                        Text(device ?? 'Unknown'),
-                        Text(date.toString()),
-                      ],
-                    );
-                  },
-                ),
-                ElevatedButton(
-                  child: const Text("Foreground Mode"),
-                  onPressed: () {
-                    print('ACTION - SAF');
-
-                    FlutterBackgroundService()
-                        .sendData({"action": "setAsForeground"});
-                  },
-                ),
-                ElevatedButton(
-                  child: const Text("Background Mode"),
-                  onPressed: () {
-                    print('ACTION - SAB');
-
-                    FlutterBackgroundService()
-                        .sendData({"action": "setAsBackground"});
-                  },
-                ),
-                ElevatedButton(
-                  child: Text(text),
-                  onPressed: () async {
-                    print('ACTION - SS');
-
-                    final service = FlutterBackgroundService();
-
-                    var isRunning = await service.isRunning();
-                    if (isRunning) {
-                      service.sendData(
-                        {"action": "stopService"},
-                      );
-                    } else {
-                      print("GOOD");
-                      service.startService();
-                    }
-
-                    if (!isRunning) {
-                      text = 'Stop Service';
-                    } else {
-                      text = 'Start Service';
-                    }
-                  },
-                ),
-              ],
             ),
           ),
         ],
