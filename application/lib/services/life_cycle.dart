f2cf73b2fc8fe51128969815272ee48be04d8f3b
Minyong Oh <dhalsdyd@gmail.com>
2022-04-16 20:14:43 +0900

    Simplify life cycle

--- application/lib/services/life_cycle.dart
+++ application/lib/services/life_cycle.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:get/get.dart';
-import 'package:soup/controllers/user_controller.dart';
 
 class LifeCycleController extends SuperController {
   @override
@@ -9,10 +8,7 @@ class LifeCycleController extends SuperController {
   @override
   void onInactive() {
     Timer.periodic(const Duration(seconds: 5), (timer) {
-      final userController = Get.find<UserController>();
-      print(userController.user.isTradeOn.toString());
-      print(userController.user.id.toString());
-      print(userController.user.email.toString());
+      print("Current : ${DateTime.now()}");
     });
   }
 
