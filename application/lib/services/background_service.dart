299f1337ccf7ce1de959774498611b698fe260df
Minyong Oh <dhalsdyd@gmail.com>
2022-04-20 17:43:12 +0900

    Restore : background-service

---
+++ application/lib/services/background_service.dart
@@ -0,0 +1,167 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:flutter_background_service_android/flutter_background_service_android.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+Future<void> initializeService() async {
+  final service = FlutterBackgroundService();
+  await service.configure(
+    androidConfiguration: AndroidConfiguration(
+      // this will executed when app is in foreground or background in separated isolate
+      onStart: onStart,
+      // auto start service
+      autoStart: true,
+      isForegroundMode: true,
+    ),
+    iosConfiguration: IosConfiguration(
+      // auto start service
+      autoStart: true,
+      // this will executed when app is in foreground in separated isolate
+      onForeground: onStart,
+      // you have to enable background fetch capability on xcode project
+      onBackground: onIosBackground,
+    ),
+  );
+  service.startService();
+}
+
+// to ensure this executed
+// run app from xcode, then from xcode menu, select Simulate Background Fetch
+bool onIosBackground(ServiceInstance service) {
+  WidgetsFlutterBinding.ensureInitialized();
+  print('FLUTTER BACKGROUND FETCH');
+
+  Timer.periodic(const Duration(seconds: 1), (timer) {
+    print('IOS FLUTTER BACKGROUND FETCH : ${DateTime.now()}');
+  });
+
+  return true;
+}
+
+void onStart(ServiceInstance service) {
+  if (service is AndroidServiceInstance) {
+    service.on('setAsForeground').listen((event) {
+      service.setAsForegroundService();
+    });
+
+    service.on('setAsBackground').listen((event) {
+      service.setAsBackgroundService();
+    });
+  }
+
+  service.on('stopService').listen((event) {
+    service.stopSelf();
+  });
+
+  // bring to foreground
+  Timer.periodic(const Duration(seconds: 1), (timer) async {
+    if (service is AndroidServiceInstance) {
+      service.setForegroundNotificationInfo(
+        title: "My App Service",
+        content: "Updated at ${DateTime.now()}",
+      );
+    }
+
+    /// you can see this log in logcat
+    print('FLUTTER BACKGROUND FETCH : ${DateTime.now()}');
+
+    // test using external plugin
+    service.invoke(
+      'update',
+      {
+        "current_date": DateTime.now().toIso8601String(),
+      },
+    );
+  });
+}
+
+_showToast(String content) => Fluttertoast.showToast(
+    msg: content,
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.BOTTOM,
+    timeInSecForIosWeb: 1,
+    backgroundColor: const Color(0xE6FFFFFF),
+    textColor: Colors.black,
+    fontSize: 13.0);
+
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   String text = "Stop Service";
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Service App'),
+//         ),
+//         body: Column(
+//           children: [
+//             StreamBuilder<Map<String, dynamic>?>(
+//               stream: FlutterBackgroundService().on('update'),
+//               builder: (context, snapshot) {
+//                 if (!snapshot.hasData) {
+//                   return const Center(
+//                     child: CircularProgressIndicator(),
+//                   );
+//                 }
+
+//                 final data = snapshot.data!;
+//                 String? device = data["device"];
+//                 DateTime? date = DateTime.tryParse(data["current_date"]);
+//                 return Column(
+//                   children: [
+//                     Text(device ?? 'Unknown'),
+//                     Text(date.toString()),
+//                   ],
+//                 );
+//               },
+//             ),
+//             ElevatedButton(
+//               child: const Text("Foreground Mode"),
+//               onPressed: () {
+//                 FlutterBackgroundService().invoke("setAsForeground");
+//               },
+//             ),
+//             ElevatedButton(
+//               child: const Text("Background Mode"),
+//               onPressed: () {
+//                 FlutterBackgroundService().invoke("setAsBackground");
+//               },
+//             ),
+//             ElevatedButton(
+//               child: Text(text),
+//               onPressed: () async {
+//                 final service = FlutterBackgroundService();
+//                 var isRunning = await service.isRunning();
+//                 if (isRunning) {
+//                   service.invoke("stopService");
+//                 } else {
+//                   service.startService();
+//                 }
+
+//                 if (!isRunning) {
+//                   text = 'Stop Service';
+//                 } else {
+//                   text = 'Start Service';
+//                 }
+//                 setState(() {});
+//               },
+//             ),
+//           ],
+//         ),
+//         floatingActionButton: FloatingActionButton(
+//           onPressed: () {},
+//           child: const Icon(Icons.play_arrow),
+//         ),
+//       ),
+//     );
+//   }
+// }
